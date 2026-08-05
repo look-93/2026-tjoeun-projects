9873c8fb70cbc4ec9a721089b2c18064c72b65ce
hahy0117 <hahy0117@gmail.com>
2026-08-05 09:29:52 +0900

    Entity 수정

--- moit-v3/back/src/main/java/com/moit/meetup/entity/MeetupImage.java
+++ moit-v3/back/src/main/java/com/moit/meetup/entity/MeetupImage.java
@@ -1,7 +1,6 @@
 package com.moit.meetup.entity;
 
-import java.awt.Image;
-
+import com.moit.common.Image;
 import com.moit.util.BaseEntity;
 
 import jakarta.persistence.Column;
@@ -25,17 +24,17 @@ import lombok.Setter;
 @Builder
 @Table(name="MEETUP_IMAGES")
 public class MeetupImage extends BaseEntity{
-	
-	@Id
+   
+   @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(unique = true, nullable = false)
-	private Long id;
-	
+   @Column(unique = true, nullable = false)
+   private Long id;
+   
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEETUP_ID", nullable = false)
-	private Meetup meetup;
-	
-	
+   private Meetup meetup;
+   
+   
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "IMAGE_ID", nullable = false)
     private Image image;
