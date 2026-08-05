09da69391611afbb3ac836a8b88191541d382911
look-93 <qhfk0961@gmail.com>
2026-08-05 16:54:19 +0900

    feat: wip: meetup service

---
+++ moit-v3/back/src/main/java/com/moit/meetup/dto/MeetupCountDto.java
@@ -0,0 +1,16 @@
+package com.moit.meetup.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MeetupCountDto {
+	// 통계데이터
+    private Long totalMeetupCount;	// 전체모집글
+    private Long recruitingCount;	// 모집중
+    private Long closedCount;		// 모집마감
+    private Long weatherCanceledCount;	// 날씨로 인한 취소
+}
