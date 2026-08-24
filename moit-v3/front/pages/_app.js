643693834793d8ec70d3b58ff49633c74c2b4df0
look-93 <qhfk0961@gmail.com>
2026-08-24 17:26:00 +0900

    feat:_app.js 충돌해결

--- moit-v3/front/pages/_app.js
+++ moit-v3/front/pages/_app.js
@@ -1,9 +1,6 @@
 //전체앱의 공통 설정(Redux Provider, 글로벌 스타일 등)
 import React, { useEffect } from "react"; // React 불러오기
 import Head from "next/head";
-import { useDispatch } from "react-redux";
-import { getMyInfoRequest } from "../reducers/userReducer";
-
 import { wrapper } from "../store/configureStore"; // Redux Store를 연결해주는 객체 ,치킨집(전역상태 + 서버연동)
 import UserLayout from "../components/layout/UserLayout"; // 공통레이아웃
 import AdminLayout from "../components/layout/AdminLayout"; // 공통레이아웃
